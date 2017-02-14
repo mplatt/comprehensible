bff8cad672718d878980d8f53b47d89463721728
Moritz Platt <mplatt02@mail.bbk.ac.uk>
2017-02-14 22:36:37 +0000

    WIP: use `big-integer` library for input sizes exceeding javascript's number range

--- ts/main/comprehensifier.ts
+++ ts/main/comprehensifier.ts
@@ -1,13 +1,13 @@
 export abstract class Comprehensifier {
 	comprehensify(message: Uint8Array): string {
 		Comprehensifier.ensureDictionarylength(this.getWords());
-		return this.join(this.toDigits(message));
+		return this.join(this.toDigits(Comprehensifier.toBigInteger(message)));
 	}
 
 	uncomprehensify(message: string): Uint8Array {
 		Comprehensifier.ensureValidMessage(message);
 		Comprehensifier.ensureDictionarylength(this.getWords());
-		return this.fromDigits(this.split(message));
+		return Comprehensifier.fromBigInteger(this.fromDigits(this.split(message)));
 	}
 
 	abstract getWords(): Array<string>
@@ -16,38 +16,39 @@ export abstract class Comprehensifier {
 
 	abstract join(words: Array<string>): string
 
-	private toDigits(data: BigInteger): Array<string> {
-		const base = this.getWords().length;
+	private toDigits(data: BigInteger, dictionary: Array<string> = this.getWords()): Array<string> {
+		const base = dictionary.length;
 		let r = data.mod(base);
-		const result = [this.getWords()[r.toJSNumber()]];
+		const result = [dictionary[r.toJSNumber()]];
 		let q = data.divide(base);
 
 		while (!q.equals(0)) {
 			r = q.mod(base);
 			q = q.divide(base);
-			result.unshift(this.getWords()[r.toJSNumber()])
+			result.unshift(dictionary[r.toJSNumber()])
 		}
 		return result;
 	}
 
-	private fromDigits(data: Array<string>): BigInteger {
-		let value = bigInt(0);
+	private fromDigits(data: Array<string>, dictionary: Array<string> = this.getWords()): BigInteger {
+		let value = bigInt.zero;
 
 		for (let word in data) {
-			value = value.multiply(this.getWords().length).add(this.getWords().indexOf(word));
+			value = value.multiply(dictionary.length).add(dictionary.indexOf(word));
 		}
 
 		if (value.lesser(0)) {
-			return bigInt(0);
+			return bigInt.zero;
 		}
 
 		let r = value.mod(10);
 		let result = r;
 		let q = value.divide(10);
 
-		while(!q.equals(0)) {
-			r = q.mod(10);
-			q = q.divide(10);
+		while (!q.equals(0)) {
+			let d = q.divmod(10);
+			r = d.remainder;
+			q = d.quotient;
 			result = result.plus(r)
 		}
 
@@ -64,5 +65,17 @@ export abstract class Comprehensifier {
 		if (message.length < 1) {
 			throw new Error("Invalid message provided");
 		}
+	}
+
+	private static toBigInteger(message: Uint8Array): BigInteger {
+		let hex = Array.from(message).map(function (value) {
+			return value.toString(16);
+		}).join("");
+
+		return bigInt(hex, 16);
+	}
+
+	private static fromBigInteger(number: BigInteger): Uint8Array {
+		return undefined;
 	}
 }
